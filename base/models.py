30610f438de1a787b19fc3c4dfd5358c17cc5b24
Ashley Wright <ashleyelainewright@gmail.com>
2019-03-21 08:22:31 -0500

    trying to figure out home page vs blog page

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db import models
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.tags import ClusterTaggableManager
@@ -33,6 +34,32 @@ class HomePage(Page):
         FieldPanel('page_title_override'),
         StreamFieldPanel('page_content'),
     ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        # Get all posts
+        all_posts = PostPage.objects.live().public().order_by('-first_published_at')
+        # Paginate all posts by 2 per page
+        paginator = Paginator(all_posts, 2)
+        # Try to get the ?page=x value
+        page = request.GET.get("page")
+        try:
+            # If the page exists and the ?page=x is an int
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If the ?page=x is not an int; show the first page
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If the ?page=x is out of range (too high most likely)
+            # Then return the last page
+            posts = paginator.page(paginator.num_pages)
+
+        # "posts" will have child pages; you'll need to use .specific in the template
+        # in order to access child properties, such as youtube_video_id and subtitle
+        context["posts"] = posts
+        return context
+
 
 
 class BasePage(Page):
